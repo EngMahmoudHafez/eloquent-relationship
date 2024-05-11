acab9033ca913a02166ed8d48cc192fff38d171f
mahmoudhafez103 <hafz7062@gmail.com>
2024-05-11 22:20:43 +0300

    add morphMap

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Eloquent\Relations\Relation;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Relation::morphMap([
+            'series' => 'App\Models\series',
+            'collection' => 'App\Models\Collection'
+        ]);
     }
 }
